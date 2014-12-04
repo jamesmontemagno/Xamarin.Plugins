b97b2e3eefeed650afe88d177914ad5c7a504d24
James Montemagno <james.montemagno@gmail.com>
2014-12-04 13:47:26 -0800

    Dispose of vibrate service on android
    bump versions

--- Vibrate/Refractored.Xam.Forms.Vibrate.Android/Vibrate.cs
+++ Vibrate/Refractored.Xam.Forms.Vibrate.Android/Vibrate.cs
@@ -25,23 +25,28 @@ namespace Refractored.Xam.Forms.Vibrate.Droid
     /// <param name="milliseconds">Time in MS (500ms is default).</param>
     public void Vibration(int milliseconds = 500)
     {
-      var v = (Vibrator)Xamarin.Forms.Forms.Context.GetSystemService(Context.VibratorService);
-
-      if (!v.HasVibrator){
-        Console.WriteLine("Android device does not have vibrator.");
-        return;
-      }
-
-      if (milliseconds < 0)
-        milliseconds = 0;
-
-      try {
-        v.Vibrate(milliseconds);
-      }
-      catch(Exception ex)
+      using (var v = (Vibrator)Xamarin.Forms.Forms.Context.GetSystemService(Context.VibratorService))
       {
-        Console.WriteLine("Unable to vibrate Android device, ensure VIBRATE permission is set.");
+
+        if (!v.HasVibrator)
+        {
+          Console.WriteLine("Android device does not have vibrator.");
+          return;
+        }
+
+        if (milliseconds < 0)
+          milliseconds = 0;
+
+        try
+        {
+          v.Vibrate(milliseconds);
+        }
+        catch (Exception ex)
+        {
+          Console.WriteLine("Unable to vibrate Android device, ensure VIBRATE permission is set.");
+        }
       }
+      
     }
   }
 }
