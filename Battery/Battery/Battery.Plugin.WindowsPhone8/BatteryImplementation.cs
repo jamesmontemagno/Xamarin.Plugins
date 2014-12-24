84627e612e31be9bcf0de3a346019bc63320962b
James Montemagno <james.montemagno@gmail.com>
2014-12-24 14:51:40 -0800

    Start implementing WP/Store

--- Battery/Battery/Battery.Plugin.WindowsPhone8/BatteryImplementation.cs
+++ Battery/Battery/Battery.Plugin.WindowsPhone8/BatteryImplementation.cs
@@ -1,4 +1,5 @@
 using Battery.Plugin.Abstractions;
+using Microsoft.Phone.Info;
 using System;
 
 
@@ -11,17 +12,38 @@ namespace Battery.Plugin
   {
     public override int Level
     {
-      get { throw new NotImplementedException(); }
+      get 
+      {
+        throw new NotImplementedException();
+      }
     }
 
     public override BatteryStatus Status
     {
-      get { throw new NotImplementedException(); }
+      get 
+      {
+        switch (DeviceStatus.PowerSource)
+        {
+          case PowerSource.Battery:
+            return BatteryStatus.Discharging;
+          default:
+            return BatteryStatus.Charging;
+        }
+      }
     }
 
     public override ChargeType ChargeType
     {
-      get { throw new NotImplementedException(); }
+      get 
+      {
+        switch (DeviceStatus.PowerSource)
+        {
+          case PowerSource.Battery:
+            return ChargeType.None;
+          default:
+            return ChargeType.Ac;
+        }
+      }
     }
   }
 }
--- Battery/Battery/Battery.Plugin.WindowsPhone81/BatteryImplementation.cs
+++ Battery/Battery/Battery.Plugin.WindowsPhone81/BatteryImplementation.cs
@@ -9,19 +9,43 @@ namespace Battery.Plugin
   /// </summary>
   public class BatteryImplementation : BaseCrossBattery
   {
+    public BatteryImplementation()
+    {
+      DefaultBattery.RemainingChargePercentChanged += (sender, args) =>
+        {
+          OnBatteryChanged(new BatteryChangedEventArgs
+            {
+              Level = DefaultBattery.RemainingChargePercent,
+              IsLow = DefaultBattery.RemainingChargePercent <= 15,
+              ChargeType = Abstractions.ChargeType.Other,
+              Status = BatteryStatus.Unknown
+            });
+        }; 
+    }
+    Windows.Phone.Devices.Power.Battery battery;
+    private Windows.Phone.Devices.Power.Battery DefaultBattery
+    {
+      get { return battery ?? (battery = Windows.Phone.Devices.Power.Battery.GetDefault()); }
+    }
     public override int Level
     {
-      get { throw new NotImplementedException(); }
+      get 
+      {
+        return DefaultBattery.RemainingChargePercent;
+      }
     }
 
     public override BatteryStatus Status
     {
-      get { throw new NotImplementedException(); }
+      get 
+      {
+        return BatteryStatus.Unknown;
+      }
     }
 
     public override ChargeType ChargeType
     {
-      get { throw new NotImplementedException(); }
+      get { return ChargeType.None; }
     }
   }
 }
