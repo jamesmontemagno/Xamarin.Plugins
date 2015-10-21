c0613508a79a951d2ca5ed92b4393e9484515eca
James Montemagno <james.montemagno@gmail.com>
2015-10-21 10:30:42 -0700

    latest nugets

--- ImageCircle/ImageCircle.Forms.Plugin.WindowsPhone/Properties/AssemblyInfo.cs
+++ ImageCircle/ImageCircle.Forms.Plugin.WindowsPhone/Properties/AssemblyInfo.cs
@@ -32,6 +32,6 @@ using System.Resources;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.1.5")]
-[assembly: AssemblyFileVersion("1.1.5")]
+[assembly: AssemblyVersion("1.1.6")]
+[assembly: AssemblyFileVersion("1.1.6")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
