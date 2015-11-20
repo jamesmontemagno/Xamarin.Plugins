7fe8d94cf431481f98172ba0fa093edb9dfaa961
James Montemagno <james.montemagno@gmail.com>
2015-11-20 17:59:56 -0500

    FInalize permissions for 1.0.0

--- Permissions/PermissionsTest/PermissionsTest.Droid/Properties/AssemblyInfo.cs
+++ Permissions/PermissionsTest/PermissionsTest.Droid/Properties/AssemblyInfo.cs
@@ -26,8 +26,8 @@ using Android.App;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1")]
-[assembly: AssemblyFileVersion("0.0.1")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
 
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
