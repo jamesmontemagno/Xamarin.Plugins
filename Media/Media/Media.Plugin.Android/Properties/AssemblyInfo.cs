0942119e8873696fa0ced477828fc7f04256874e
James Montemagno <james.montemagno@gmail.com>
2015-11-20 17:49:42 -0500

    Add default permissions.

--- Media/Media/Media.Plugin.Android/Properties/AssemblyInfo.cs
+++ Media/Media/Media.Plugin.Android/Properties/AssemblyInfo.cs
@@ -28,3 +28,9 @@ using Android.App;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2.0.0")]
 [assembly: AssemblyFileVersion("2.0.0")]
+
+
+//Permissions for android
+[assembly: UsesPermission(Android.Manifest.Permission.Camera)]
+[assembly: UsesPermission(Android.Manifest.Permission.WriteExternalStorage)]
+[assembly: UsesPermission("android.permission.READ_EXTERNAL_STORAGE")]
