229c6f531d8318d52d2a55129d4de4e36bd45e63
Alberto <alesvi@gmail.com>
2016-01-27 23:30:45 +0000

    RestClient Plugin. First commit. Solution template and Interface.

---
+++ RestClient/Plugin.RestClient.iOSUnified/RestClientImplementation.cs
@@ -0,0 +1,18 @@
+using Plugin.RestClient.Abstractions;
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Plugin.RestClient
+{
+    /// <summary>
+    /// Implementation for RestClient
+    /// </summary>
+    public class RestClientImplementation : IRestClient
+    {
+        public Task<HttpResponseMessage> SendAsync(HttpRequestMessage request)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
